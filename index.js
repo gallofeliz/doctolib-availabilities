5333cf4fb899b66d6cc5c85ad2d6c16330abb061
Gallo Feliz <40487446+gallofeliz@users.noreply.github.com>
2021-09-24 20:33:59 +0200

    RPI relou !!

--- index.js
+++ index.js
@@ -24,10 +24,12 @@ async function run() {
             availabilities[config.id] = false;
         }
 
+        console.log('Opening page')
         const page = await browser.newPage();
-
+        console.log('Set useragent')
         await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
 
+        console.log('Set viewport')
         await page.setViewport({ width: 1280, height: 800 })
 
         async function getAvail() {
